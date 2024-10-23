79e52354c39660ece3f3d24233ba89133559f845
Victor Mendes <victorhgabriel3@gmail.com>
2024-10-22 22:53:40 -0300

    Primeiro projeto NodeJS_Web

---
+++ NodeJS_web/Exercicio - 01/src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("node:path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
+let users = [];
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/register", (req, res) => {
+  const { username, email } = req.body;
+
+  if (!username || !email) {
+    res.redirect("/");
+    return res.status(400).send("Por favor, preencha todos os campos.");
+  } else {
+    users.push({ username, email });
+    res.redirect("/success");
+  }
+});
+
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.get("/users", (req, res) => {
+  res.render("users", { users: users });
+});
+
+app.post("/users/delete", (req, res) => {
+  const { username } = req.body;
+  users = users.filter((user) => user.username !== username);
+  res.redirect("/users");
+});
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Servidor rodando em: http://localhost:${PORT}/`)
+);
